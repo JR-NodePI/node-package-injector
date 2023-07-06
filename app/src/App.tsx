3acdcc599355443fc0ca5abfe7b3a3e04819fef3
JorgeRojo <jrojo@redpoints.com>
2023-07-06 04:36:09 +0200

    Add electron with vite for react

---
+++ app/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import logo from "./assets/logo.svg";
+
+function App() {
+  return (
+    <div>
+      <img src={logo} className="logo" alt="NodePI" />
+      <h1>NodePI</h1>
+      <h2>Node Package Injector</h2>
+    </div>
+  );
+}
+
+export default App;
