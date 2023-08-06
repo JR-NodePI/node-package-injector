19fc56f3bd4db723c64472da387f9269e7cf8a14
Jorge Rojo <jorge.rojodiseno@gmail.com>
2023-08-06 05:59:40 +0200

    Fix PackageScripts memory leak

---
+++ src/renderer/src/appComponents/PackageScripts/components/PackageScriptRendererProps.ts
@@ -0,0 +1,17 @@
+import { PackageScript } from '@renderer/models/PackageScriptsTypes';
+import { Form } from 'fratch-ui';
+
+export type PackageScriptRendererProps = {
+  index: number;
+  onAdd: () => void;
+  onChange: (index: number, script?: PackageScript) => void;
+  onRemove: (index: number) => void;
+  script: PackageScript;
+  scriptOptions: Form.SelectProps.SelectOption<PackageScript>[];
+  showAddButton: boolean;
+};
+
+export type PackageScriptButtonsProps = Pick<
+  PackageScriptRendererProps,
+  'index' | 'onAdd' | 'onRemove' | 'showAddButton'
+>;
