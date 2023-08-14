f5d1094e9bb98c7f9f3668260efa574a4b5f7218
JorgeRojo <jrojo@redpoints.com>
2023-08-14 02:44:00 +0200

    v1.3.32

--- src/renderer/src/constants.ts
+++ src/renderer/src/constants.ts
@@ -1,5 +1,5 @@
-import { COLOR_LIST_FOR_RANDOMIZE } from 'fratch-ui/constants';
+import { COLOR_LIST } from 'fratch-ui/constants';
 
 export const TABS_MINIMUM_REMOVABLE = 1;
-export const TABS_MAXIMUM_ADDABLE = COLOR_LIST_FOR_RANDOMIZE.length;
+export const TABS_MAXIMUM_ADDABLE = COLOR_LIST.length;
 export const TAB_TITLE_TEMPLATE = 'Package {{number}}';
