24a6c64561cf7eaa591fef3ef0638f4d6893dea7
jorgeRojo <jorge.rojodiseno@gmail.com>
2023-09-26 15:47:11 +0200

    =Add vitest

--- src/renderer/src/services/TerminalService.ts
+++ src/renderer/src/services/TerminalService.ts
@@ -1,5 +1,9 @@
 const TerminalService = window.api.TerminalService;
 
+export type ExecuteCommandOptions = Parameters<
+  typeof TerminalService.executeCommand
+>[0];
+
 export type TerminalResponse = Awaited<
   ReturnType<typeof TerminalService.executeCommand>
 >;
---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.ts',
+    coverage: {
+      all: true,
+      include: ['src/**/*.ts', 'src/**/*.tsx'],
+      exclude: [
+        '**/__mocks__/**/*.{tsx,ts}',
+        '**/__tests__/**/*.{tsx,ts}',
+        '**/*.d.ts',
+        '**/*.test.{tsx,ts}',
+      ],
+      statements: 19,
+      branches: 48,
+      functions: 34,
+      lines: 19,
+      reporter: ['text', 'json-summary', 'html', 'json'],
+    },
+  },
+});
