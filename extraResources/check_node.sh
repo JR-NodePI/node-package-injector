f35a2bbf14b3377f5b175c8752e93ee02f547540
Jorge Rojo <jorge.rojodiseno@gmail.com>
2023-11-07 23:18:42 +0100

    v1.7.11-alpha

--- extraResources/check_node.sh
+++ extraResources/check_node.sh
@@ -7,16 +7,23 @@ check_command "rsync"
 check_command "shasum"
 check_command "watch"
 
-NODE_VERSION=$(node -v)
-NPM_VERSION=$(npm -v)
-YARN_VERSION=$(yarn -v)
-PNPM_VERSION=$(pnpm -v)
-NVM_VERSION=$(nvm -v)
+get_version() {
+  local commandName=$1
+  local commandAlias=$(command -v "${commandName}")
+  if [[ -n "${commandAlias}" ]]; then
+    eval "$commandName -v"
+  else
+    echo ""
+  fi
+}
+
+NODE_VERSION=$(get_version "node")
+NPM_VERSION=$(get_version "npm")
+YARN_VERSION=$(get_version "yarn")
+PNPM_VERSION=$(get_version "pnpm")
+NVM_VERSION=$(get_version "nvm")
 
 echo "{
-  \"nvm-bin\": \"$(command -v nvm)\",
-  \"node-bin\": \"$(command -v node)\",
-  \"npm-bin\": \"$(command -v npm)\",
   \"cwd\": \"$(pwd)\",
   \"node\": \"$NODE_VERSION\",
   \"npm\":  \"$NPM_VERSION\",
