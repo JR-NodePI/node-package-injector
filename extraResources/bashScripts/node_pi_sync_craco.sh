d49a2af0f362f406fa1ed337657828e68c8743ba
jorgeRojo <jorge.rojodiseno@gmail.com>
2023-11-27 19:33:11 +0100

    Craco sync is alive

--- extraResources/bashScripts/node_pi_sync_craco.sh
+++ extraResources/bashScripts/node_pi_sync_craco.sh
@@ -42,49 +42,88 @@ echo ""
 
 mkdir -p "${NODE_PI_FILE_PREFIX}"
 
-create_vite_config_for_sync_dependencies() {
+create_craco_config_for_sync_dependencies() {
   echo "
 /* eslint-disable */
 const path = require('path');
-const craco = require('@craco/craco');
+const { getLoader, loaderByName } = require('@craco/craco');
+const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
+
 const config = require('../$CRACO_CONFIG_FILE_NAME');
 const nodePiSyncModules = [$NODE_PI_DEPENDENCY_NAMES];
 
+const packages = nodePiSyncModules
+  .map(dep => path.resolve(__dirname, dep));
 
-const packages = nodePiSyncModules.map(dep => path.resolve(__dirname, dep));
+const packagesAlias = Object.fromEntries(
+  nodePiSyncModules
+    .map(dep => [
+      [dep, path.resolve(__dirname, dep)],
+      [\`~\${dep}\`, path.resolve('./node_modules', dep)],
+    ])
+    .flat()
+);
 
-config.webpack = {
-  ...(config?.webpack ?? {}),
-  alias: {
-    ...(config?.webpack?.alias ?? {}),
-    ...Object.fromEntries(
-      nodePiSyncModules.map(dep => [dep, path.resolve(__dirname, dep)])
-    ),
-  },
-  configure: webpackConfig => {
-    const { isFound, match } = craco.getLoader(
+const sanitizeRules = rules =>
+  rules.map(rule => {
+    if (rule.use) {
+      rule.use = !Array.isArray(rule.use)
+        ? [rule.use]
+        : sanitizeRules(rule.use);
+    } else if (rule.oneOf) {
+      rule.oneOf = sanitizeRules(rule.oneOf);
+    } else if (Array.isArray(rule.loader)) {
+      rule.loader = sanitizeRules(rule.loader);
+    }
+
+    return rule;
+  });
+
+const NodePiInjectPlugin = {
+  overrideWebpackConfig: ({ webpackConfig }) => {
+    //--- alias
+    webpackConfig.resolve.alias = {
+      ...(webpackConfig.resolve.alias ?? {}),
+      ...packagesAlias,
+    };
+
+    //--- scope
+    const moduleScopePlugin = (webpackConfig.resolve.plugins ?? []).find(
+      plugin => plugin instanceof ModuleScopePlugin
+    );
+    if (moduleScopePlugin) {
+      moduleScopePlugin.appSrcs = [
+        ...(moduleScopePlugin.appSrcs ?? []),
+        ...packages,
+      ];
+    }
+
+    //--- loader
+    webpackConfig.module.rules = sanitizeRules(webpackConfig.module.rules);
+
+    const { isFound, match } = getLoader(
       webpackConfig,
-      craco.loaderByName('babel-loader')
+      loaderByName('babel-loader')
     );
 
     if (isFound) {
-      const include = Array.isArray(match.loader.include)
-        ? match.loader.include
-        : [match.loader.include];
-      match.loader.include = include.concat(packages);
+      match.loader.include = Array.isArray(match.loader.include)
+        ? [...match.loader.include, ...packages]
+        : [match.loader.include, ...packages];
     }
 
     return webpackConfig;
   },
 };
 
-module.exports = config;
+config.plugins = [...config.plugins, { plugin: NodePiInjectPlugin }];
 
+module.exports = config;
 
 " >"${NODE_PI_FILE_PREFIX}/${CRACO_CONFIG_FILE_NAME}"
 }
 
-replace_vite_config_file_in_package_json() {
+replace_craco_config_file_in_package_json() {
   local PATTERN_SEC_1='(:.*craco.*start)'                                                   # find the vite section inside a package.json script
   local PATTERN_SEC_2="((.*)--config.*\.(js|ts|cjs) *'?)?"                                  # find the optional --config inside the vite section
   local PATTERN_SEC_3='(([^&]+&&)?[^"]*")'                                                  # find the end of the package.json script line
@@ -98,5 +137,5 @@ replace_vite_config_file_in_package_json() {
 
 export default config
 
-create_vite_config_for_sync_dependencies
-replace_vite_config_file_in_package_json
+create_craco_config_for_sync_dependencies
+replace_craco_config_file_in_package_json
