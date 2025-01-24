3f2026a62353c23b60f1ec8059b129fc4f8d017b
jorgeRojo <jorge.rojodiseno@gmail.com>
2025-01-24 13:41:19 +0100

    Improve SecureWinBounds

--- src/main/SecureWinBounds.ts
+++ src/main/SecureWinBounds.ts
@@ -1,37 +1,57 @@
+import { screen } from 'electron';
 import Config from 'electron-config';
 
 const config = new Config();
 
 export default class SecureWinBounds {
-  private static _defaultWinBounds: Electron.Rectangle = {
-    x: 25,
-    y: 20,
-    width: 800,
-    height: 600,
-  };
+  private static _defaultWinWidth = 800;
+  private static _defaultWinHeight = 600;
 
   static get(): Electron.Rectangle {
+    const { workArea } = screen.getPrimaryDisplay();
     const winBounds: Electron.Rectangle = config.has('winBounds')
       ? config.get('winBounds')
-      : SecureWinBounds._defaultWinBounds;
+      : {
+          ...workArea,
+          width: SecureWinBounds._defaultWinWidth,
+          height: SecureWinBounds._defaultWinHeight,
+        };
+
+    const secureX = (winBounds?.x ?? 0) < workArea.x ? workArea.x : winBounds.x;
+
+    let secureWidth =
+      (winBounds?.width ?? 0) < SecureWinBounds._defaultWinWidth
+        ? SecureWinBounds._defaultWinWidth
+        : winBounds.width;
+
+    const secureY = (winBounds?.y ?? 0) < workArea.y ? workArea.y : winBounds.y;
+
+    let secureHeight =
+      (winBounds?.height ?? 0) < SecureWinBounds._defaultWinHeight
+        ? SecureWinBounds._defaultWinHeight
+        : winBounds.height;
+
+    if (secureX + secureWidth > workArea.width) {
+      secureWidth = workArea.width - secureX;
+    }
+
+    if (secureWidth < SecureWinBounds._defaultWinWidth) {
+      secureWidth = SecureWinBounds._defaultWinWidth;
+    }
+
+    if (secureY + secureHeight > workArea.height) {
+      secureHeight = workArea.height - secureY;
+    }
+
+    if (secureHeight < SecureWinBounds._defaultWinHeight) {
+      secureHeight = SecureWinBounds._defaultWinHeight;
+    }
 
     return {
-      x:
-        (winBounds?.x ?? 0) < SecureWinBounds._defaultWinBounds.x
-          ? SecureWinBounds._defaultWinBounds.x
-          : winBounds.x,
-      y:
-        (winBounds?.y ?? 0) < SecureWinBounds._defaultWinBounds.y
-          ? SecureWinBounds._defaultWinBounds.y
-          : winBounds.y,
-      width:
-        (winBounds?.width ?? 0) < SecureWinBounds._defaultWinBounds.width
-          ? SecureWinBounds._defaultWinBounds.width
-          : winBounds.width,
-      height:
-        (winBounds?.height ?? 0) < SecureWinBounds._defaultWinBounds.height
-          ? SecureWinBounds._defaultWinBounds.height
-          : winBounds.height,
+      x: secureX,
+      y: secureY,
+      width: secureWidth,
+      height: secureHeight,
     };
   }
 
